8d48289270db24f7a896b00be9421ff1b1e997f8
Student <example@example.com>
2019-10-31 16:45:43 -0700

    fix some variable names.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,30 @@
 var finalProduct = [];
 var input = ("I like to wear socks in the morning!");
 console.log(input);
-
+var lowerArray = [];
+var lowerArray2 = [];
 function prepLetters(sentence){
   var lowerSentence = sentence.toLowerCase();
   var re = /[,.!?;'":><%$&*@#1234567890 ]/g;
+  lowerSentence.split("")
   return lowerSentence.replace(re, "");
-
+  console.log(lowerSentence);
 };
-
 
 function cryptoGridder(input){
   var prepInput = prepLetters(input);
+  lowerArray.push(prepInput);
   console.log(prepInput);
   var row = Math.ceil(Math.sqrt(prepInput.length));
   var column = Math.floor(Math.sqrt(prepInput.length));
 
+  console.log(lowerArray)
   for (var i = 0; i < row; i++) {
     var thisRow = [];
     for (var j = 0; j < column; j++) {
-      thisRow.push((prepInput.length < 1) ? "" :
-      prepInput.shift());
+      thisRow.push((lowerArray.length < 1) ? "" :
+      lowerArray.shift());
+      // console.log(lowerArray);
 
 
 
